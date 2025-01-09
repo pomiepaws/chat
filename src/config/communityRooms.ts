f540c4f25868c968ff1aa02529c3d6410cfa5474
pomiepaws <113963000+pomiepaws@users.noreply.github.com>
2025-01-09 14:28:36 -0500

    Update communityRooms.ts

--- src/config/communityRooms.ts
+++ src/config/communityRooms.ts
@@ -1,10 +1,10 @@
 export const communityRoomNames = [
-  'buy-and-sell',
-  'crypto',
-  'hacking',
-  'leaks',
-  'news',
-  'organize',
-  'politics',
-  'resist',
+  'work',
+  'work1',
+  'work2',
+  'work3',
+  'work4',
+  'work5',
+  'work6',
+  'work7',
 ]
